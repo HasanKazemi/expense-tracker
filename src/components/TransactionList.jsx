63db2f527639e7741db114f6e74e5a8c40862ffa
HasanKazemi <avatarhasan78@gmail.com>
2025-03-13 22:05:25 +0330

    transaction list

---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const TransactionList = () => {
+  const { state, dispatch } = useContext(AppContext);
+
+  const handleDelete = (id) => {
+    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+  };
+
+  return (
+    <div>
+      <h2>لیست تراکنش‌ها</h2>
+      <ul>
+        {state.transactions.map((transaction) => (
+          <li key={transaction.id}>
+            <span>{transaction.title}</span>
+            <span>{transaction.amount.toLocaleString()} تومان</span>
+            <span>{transaction.date}</span>
+            <span>{transaction.category}</span>
+            <button onClick={() => handleDelete(transaction.id)}>حذف</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionList;
